69684d015a8dcac5d27f7b8da4ebed52c4c8d4b0
Owen Evans <owen@bgeek.net>
2014-12-08 11:57:59 +1300

    initiate bounce method

--- tests/unit_tests/controllers/redirect_controller_tests.js
+++ tests/unit_tests/controllers/redirect_controller_tests.js
@@ -2,6 +2,9 @@
 var RedirectController = require('../../../lib/controllers/redirect_controller');
 var sinon = require('sinon');
 var expect = require('chai').expect;
+var BouncerServer = require('../../../lib/server');
+var Model = require('hoist-model');
+var BBPromise = require('bluebird');
 
 describe('RedirectController', function () {
   describe('#initRoutes', function () {
@@ -16,22 +19,119 @@ describe('RedirectController', function () {
       return expect(server.route)
         .to.be.calledWith({
           handler: RedirectController.initiateBounce,
-          path: '/initiate/{key}',
+          path: '/initiate/{orgSlug}/{appSlug}/{key}',
           method: 'GET'
         });
     });
   });
-  describe('#initiateBounce', function () {
-    var response = {
-      redirect:sinon.stub()
-    };
-    var request = {};
-    before(function () {
-      RedirectController.initiateBounce(request, response);
-    });
-    it('redirects user',function(){
-      return expect(response.redirect)
-      .to.have.been.called;
+  describe('routes', function () {
+    describe('GET /initiate/{orgSlug}/{appSlug}/{key}', function () {
+      var organisation = {
+        _id: 'orgid'
+      };
+      var application = {
+        _id: 'appid'
+      };
+      var connectorSettings = {
+        application: 'appid',
+        environment: 'live',
+        key: 'connectorkey'
+      };
+      var server = BouncerServer.getServer();
+      var token = {
+        key: 'tokenkey'
+      };
+      var _response;
+      var _savedToken;
+      before(function (done) {
+        sinon.stub(Model.Organisation, 'findOneAsync')
+          .withArgs({
+            slug: 'org_slug'
+          }).returns(BBPromise.resolve(organisation));
+        sinon.stub(Model.Application, 'findOneAsync')
+          .withArgs({
+            slug: 'app_slug',
+            organisation: 'orgid'
+          }).returns(BBPromise.resolve(application));
+        sinon.stub(Model.ConnectorSetting, 'findOneAsync')
+          .withArgs({
+            application: 'appid',
+            environment: 'live',
+            key: 'my_connector_key'
+          }).returns(BBPromise.resolve(connectorSettings));
+        sinon.stub(Model.BouncerToken.prototype, 'saveAsync', function () {
+          _savedToken = this;
+          return BBPromise.resolve([token]);
+        });
+        server.inject({
+          method: 'GET',
+          url: '/initiate/org_slug/app_slug/my_connector_key'
+        }, function (response) {
+          _response = response;
+          done();
+        });
+      });
+      it('creates a bouncer token', function () {
+        return expect(_savedToken).to.exist;
+      });
+      it('redirects the user to bounce', function () {
+        return expect(_response.headers.location)
+          .to.match(/\/bounce\/tokenkey$/);
+      });
+      describe('with invalid key', function () {
+        var _response;
+        before(function (done) {
+          server.inject({
+            method: 'GET',
+            url: '/initiate/org_slug/app_slug/other_connector_key'
+          }, function (response) {
+            _response = response;
+            done();
+          });
+        });
+        it('returns 404 error', function () {
+          return expect(_response.statusCode)
+            .to.eql('404');
+        });
+      });
+      describe('with invalid appSlug', function () {
+        var _response;
+        before(function (done) {
+          server.inject({
+            method: 'GET',
+            url: '/initiate/org_slug/other_app_slug/my_connector_key'
+          }, function (response) {
+            _response = response;
+            done();
+          });
+        });
+        it('returns 404 error', function () {
+          return expect(_response.statusCode)
+            .to.eql('404');
+        });
+      });
+      describe('with invalid orgSlug', function () {
+        var _response;
+        before(function (done) {
+          server.inject({
+            method: 'GET',
+            url: '/initiate/other_org_slug/app_slug/my_connector_key'
+          }, function (response) {
+            _response = response;
+            done();
+          });
+        });
+        it('returns 404 error', function () {
+          return expect(_response.statusCode)
+            .to.eql('404');
+        });
+      });
+      after(function () {
+        Model.Organisation.findOneAsync.restore();
+        Model.Application.findOneAsync.restore();
+        Model.ConnectorSetting.findOneAsync.restore();
+        Model.BouncerToken.prototype.saveAsync.restore();
+      });
     });
   });
 });
