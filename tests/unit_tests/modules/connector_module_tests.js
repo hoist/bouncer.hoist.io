090d811bbc4d61c12a6d800154c055fd7bcc9d89
Owen Evans <owen@bgeek.net>
2014-12-09 10:22:54 +1300

    require connectors by real path
    so they don't get cached between releases

--- tests/unit_tests/modules/connector_module_tests.js
+++ tests/unit_tests/modules/connector_module_tests.js
@@ -1,17 +1,28 @@
 'use strict';
 var connectorModule = require('../../../lib/modules/connector_module');
-var TestConnector = require('../../fixtures/connectors/test-connector/current');
+var TestConnector = require('../../fixtures/connectors/test-connector/deployed');
 var Model = require('hoist-model');
 var expect = require('chai').expect;
+var path = require('path');
+var fs = require('fs');
 describe('ConnectorModule', function () {
   describe('#loadConnector', function () {
     var _connector;
+    var deployedPath = path.resolve('./tests/fixtures/connectors/test-connector/deployed');
+
+    var currentPath = path.resolve(deployedPath, '../current');
     before(function () {
+
+      //fs.mkdirSync(currentPath);
+      fs.symlinkSync(deployedPath, currentPath);
       return connectorModule.loadConnector(new Model.ConnectorSetting({
         connectorType: 'test-connector'
       })).then(function (connector) {
         _connector = connector;
       });
+    });
+    after(function(){
+      fs.unlinkSync(currentPath);
     });
     it('returns instance of test connector', function () {
       return expect(_connector).to.be.instanceOf(TestConnector);
