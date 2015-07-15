cd6e5ddf14cabae2401bb8a6ac36e6e3a13eeb17
Owen Evans <owen@bgeek.net>
2015-07-15 14:27:39 +1200

    switch to promises

--- app.js
+++ app.js
@@ -3,18 +3,34 @@ var logger = require('hoist-logger');
 var server = require('./lib/server');
 
 process.title = 'bouncer.hoist.io';
-process.on('message', function (msg) {
-  if (msg === 'shutdown') {
-    console.log('closing connection');
-    setTimeout(function () {
-      console.log('shutting down git server');
-      server.stop(function () {
-        process.exit(0);
-      });
-    }, 500);
-  }
-});
 
-server.start(function () {
-  logger.info('server running');
+var gracefullShutdown = function (SIG) {
+  logger.info({
+    SIG: SIG
+  }, 'server stopping');
+  return Promise.all([
+    server.stop()
+  ]).then(function () {
+    process.kill(process.pid, SIG);
+  }).catch(function (err) {
+    logger.error(err);
+    logger.alert(err);
+    throw err;
+  });
+};
+server.start().then(function () {
+  logger.info('service started');
+  process.once('SIGUSR2', function () {
+    return gracefullShutdown('SIGUSR2');
+  });
+  process.once('SIGTERM', function () {
+    return gracefullShutdown('SIGTERM');
+  });
+  process.once('SIGINT', function () {
+    return gracefullShutdown('SIGINT');
+  });
+}).catch(function (err) {
+  logger.error(err);
+  logger.alert(err);
+  process.exit(1);
 });
